d37c2c6b68bc338d161a07e4c2eb0d14faa59e4f
Dylan <drossi517@gmail.com>
2019-09-19 08:16:22 -0700

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,6 +1,7 @@
-/*Dylan Rossi
-*CS4A 9.18.19
-*In Class POS System for IN N OUT
+/*        Dylan Rossi
+*         CS4A 9.18.19
+*         In-Class Assignment:
+*         POS System for IN N OUT
 */
 import java.util.*;
 
@@ -11,19 +12,17 @@ public static void main(String[] args){
      displayMenu();
 }
 
-//this function will display the menu choices and allow user to make a choice
 public static void displayMenu(){
-Scanner in = new Scanner(System.in);
+     Scanner in = new Scanner(System.in);
      //Money
      double subTotal = 0, totalGross = 0, totalNet = 0;
      //Menu Items
      double hamburger = 2.50, cheeseburger = 3.10, doubleDouble = 4.10, fries = 2.00, milkshake = 2.50, soda = 1.75;
-     //Counter, user input
+     //Order No., User Input
      int ticket = 1, choice = 0;
 
      while (choice != 8){
-               //update menu with borders and correct alignment
-          System.out.println("\t\tMENU:\n");
+          System.out.println("\t\tMENU:\n"); //update menu with borders
           System.out.println("\t1: Hamburger\t\t\t$" + hamburger);
           System.out.println("\t2: Cheeseburger\t\t\t$" + cheeseburger);
           System.out.println("\t3: Double Double\t\t$" + doubleDouble);
@@ -35,7 +34,6 @@ Scanner in = new Scanner(System.in);
           System.out.println("\t\t\t\t\tTicket No.: " + ticket);
           System.out.printf("\t\t\t\t\tSubtotal: $ %,.2f", subTotal);
           System.out.print("\n\n\t\tCHOICE:\t");
-
           choice = in.nextInt();
 
           switch(choice){
@@ -46,18 +44,17 @@ Scanner in = new Scanner(System.in);
                case 5: subTotal += getSub(milkshake, in); break;
                case 6: subTotal += getSub(soda, in); break;
                case 7: getOrder(subTotal, ticket, in); ticket++; totalNet += subTotal; subTotal = 0; break;
-               case 8: getTotal(totalGross, totalNet, ticket); break;
-          }//switch
-     }//while loop
-     in.close();
-}//method
+               //if you have an open order, and did not choose '7' prior to '8'/QUIT, you lose the current order
+               case 8: getTotal(totalGross, totalNet, ticket); in.close(); break;
+          }
+     }
+}
 
 public static double getSub(double menuChoice, Scanner in){
 
      System.out.print("\n\t\tHow many?: ");
      int howMany = in.nextInt();
      System.out.println("\n\n");
-
      double cost = menuChoice * howMany;
      return cost;
 }
@@ -65,11 +62,10 @@ public static double getSub(double menuChoice, Scanner in){
 public static void getOrder(double cost, int ticketNum, Scanner in){
 
      double totalCost = cost * TAX_RATE;
-     
      System.out.printf("\n\nThe total cost of the order is: $%,.2f", totalCost);
      System.out.println("\nYour order number is: " + ticketNum);
      System.out.print("\nAny number + ENTER to continue: ");
-     //this section needs to be cleaned up- has to be a better way to pause system and wait for input of any kind
+     //this line needs to be cleaned up- has to be a better way to pause system and wait for input of any kind
      int nullVar = in.nextInt();
      System.out.println("\n\n\n\n");
 }
@@ -79,6 +75,6 @@ public static void getTotal(double totalGross, double totalNet, int ticket){
      totalGross = totalNet * TAX_RATE;
      System.out.printf("\n\n\nTotal Revenue including tax: \t$%,.2f", totalGross);
      System.out.printf("\nTotal Revenue without tax: \t$%,.2f", totalNet);
-     System.out.println("\nTotal number of orders: \t" + (ticket - 1));
+     System.out.println("\nTotal number of orders:  \t" + (ticket - 1));
 }
 }
